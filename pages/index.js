4208c129115c0dd959606d0150cd367895389a1d
Aram <aram@ftmo.com>
2022-11-30 12:37:48 +0100

    Cars test

--- pages/index.js
+++ pages/index.js
@@ -22,17 +22,6 @@ export default class Home extends Component {
             ))}
           </ul>
         </article>
-        <form name="contact" method="POST" data-netlify="true">
-          <p>
-            <label>Name <input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Email <input type="email" name="email" /></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
       </>
     )
   }
