fa8fbaeec85e6b4164a1912f10a5c9fce74407fa
Aram <aram@ftmo.com>
2022-11-30 10:52:07 +0100

    form test

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,17 @@ export default class Home extends Component {
             ))}
           </ul>
         </article>
+        <form name="contact" netlify>
+          <p>
+            <label>Name <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Email <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </>
     )
   }
